1981228f341b8dcb50a17f316970a0f7efc4f6a2
Thomas Neder <thomas.neder@freenet.de>
2023-01-15 15:33:07 +0100

    Provide RDS improvements

--- includes/fm-constants.h
+++ includes/fm-constants.h
@@ -34,9 +34,10 @@
 #define	QT_STATIC_CONST
 
 /*
-Stereo sideband test (after activating compile switch DO_STEREO_SEPARATION_TEST)
-  1) Select "S|S" to hear only the sideband signal
-  2) Check different station with the IQ balance slider whether the signal get minimal at center "0" position
+ * Stereo sideband test (after activating compile switch DO_STEREO_SEPARATION_TEST)
+ * 1) Select "T|T" to hear the ortogonal demodulated sideband signal
+ * 2) Check different station with the IQ balance slider whether the signal get minimal at center "0" position
+ * 3) You can also see in the RDS IQ scope the remaing phase (Costas Loop is switched off)
 */
 //#define	DO_STEREO_SEPARATION_TEST
 
